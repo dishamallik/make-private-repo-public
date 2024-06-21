acad907f6be78314a0d4467d32dc4f86d22e6e77
dishamallik <dishamallik758@gmail.com>
2024-06-22 02:57:04 +0600

    created simple website

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -75,19 +75,19 @@ else{
         console.log('transaction id', paymentIntent.id);
         setTransactionId(paymentIntent.id);
 
-        // // now save the payment in the database
-        // const payment = {
-        //     email: user.email,
-        //     price: totalPrice,
-        //     transactionId: paymentIntent.id,
-        //     date: new Date(), // utc date convert. use moment js to 
-        //     cartIds: cart.map(item => item._id),
-        //     menuItemIds: cart.map(item => item.menuId),
-        //     status: 'pending'
-        // }
+        // now save the payment in the database
+        const payment = {
+            email: user.email,
+            price: totalPrice,
+            transactionId: paymentIntent.id,
+            date: new Date(),
+            userId: user._id,
+            menuId: menu.map(item => item._id),
+            status: 'pending'
+        }
 
-        // const res = await axiosSecure.post('/payments', payment);
-        // console.log('payment saved', res.data);
+        const res = await axiosSecure.post('/payments', payment);
+        console.log('payment saved', res.data);
         // refetch();
         // if (res.data?.paymentResult?.insertedId) {
         //     Swal.fire({
